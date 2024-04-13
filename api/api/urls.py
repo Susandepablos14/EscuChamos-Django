8ac09f6881c964191db80df76ec4bc92e1b38c4d
susandepablos14 <susanb140903@gmail.com>
2024-04-13 17:22:02 -0400

    Cambios usuarios

--- api/api/urls.py
+++ api/api/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('logout/', UserLogoutAPIView.as_view(), name='logout'),
     path('register/', UserRegisterAPIView.as_view(), name='register'),
     path('verify-email/<str:token>/', UserVerifyAPIView.as_view(), name='verify_email'),
+    
+    path('users', UserIndexAPIView.as_view(), name='user-list'),
+    path('user/<int:pk>', UserShowAPIView.as_view(), name='user-show'),
 ]
