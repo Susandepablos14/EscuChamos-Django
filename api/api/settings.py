962dc098cd81d492d31c8399f710bfce158061b5
susandepablos14 <susanb140903@gmail.com>
2024-04-25 17:57:19 -0400

    cambios

--- api/api/settings.py
+++ api/api/settings.py
@@ -1,9 +1,11 @@
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+PORT = int(os.environ.get("PORT", 8000))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -14,9 +16,7 @@ SECRET_KEY = 'django-insecure-btlia-0@g(@qze6tnav=axz6jsi3n&-df$1=(ef$(_s*8*o1#+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'escuchamos.onrender.com'
-]
+ALLOWED_HOSTS = ['escuchamos.onrender.com', '0.0.0.0', 'localhost', '127.0.0.1']
 
 
 # Application definition
